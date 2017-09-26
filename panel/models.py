7e175d4c86ac40e419d577bb4159d8bdeefbbfc0
OlegSokol <soklakov.o.yu@gmail.com>
2017-09-26 23:43:41 +0300

    update panel model

--- panel/models.py
+++ panel/models.py
@@ -1,9 +1,21 @@
 from django.db import models
-from django.utils import timezone
+
 
 class Product(models.Model):
     product_number = models.BigIntegerField(null=False, serialize=True)
     description = models.CharField(max_length=200)
     image_id = models.CharField(max_length=200)
-    created_date = models.DateTimeField(default=timezone.now)
     price = models.BigIntegerField()
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+
+class Order(models.Model):
+    order_number = models.BigIntegerField(null=False, serialize=True)
+    customer_name = models.CharField(max_length=200)
+    customer_surname = models.CharField(max_length=200)
+    customer_email = models.CharField(max_length=200)
+    order_products = models.ManyToManyField(Product)
+    total_price = models.BigIntegerField()
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
